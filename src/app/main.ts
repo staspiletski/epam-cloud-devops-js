c7af80bcfdae13bdd77e8d2c03d908c3850ff01d
stanislau piletski <s.piletski@napoleongames.be>
2021-05-08 11:33:54 +0300

    feat: hw2

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import quotesRouter from '../app/routes/quotes';
-import path from 'path';
 import methodOverride from 'method-override';
 
 dotenv.config();
 
-const APP_PORT = process.env.APP_PORT || 8001;
+const APP_PORT = process.env.APP_PORT || 4201;
 const main: express.Application = express();
 
 main.use(express.json());
@@ -15,8 +14,6 @@ main.use(express.urlencoded({ extended: false }));
 main.use(methodOverride('_method'));
 
 main.use(quotesRouter);
-
-main.use('/static', express.static(path.join(__dirname, '../../', 'static')));
 
 main.listen(APP_PORT, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${APP_PORT}`);
