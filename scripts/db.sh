df687ea76fa94f45b54edb3f0860194e5065b126
stanislau piletski <s.piletski@napoleongames.be>
2021-06-01 22:00:32 +0300

    feat: init hw3

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,114 +1,105 @@
 #!/bin/bash
 
-BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-LIME_YELLOW=$(tput setaf 190)
-POWDER_BLUE=$(tput setaf 153)
-BLUE=$(tput setaf 4)
-MAGENTA=$(tput setaf 5)
-CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
-BRIGHT=$(tput bold)
-NORMAL=$(tput sgr0)
-BLINK=$(tput blink)
-REVERSE=$(tput smso)
-UNDERLINE=$(tput smul)
+source colors.sh
 
-function print_available_commands() {
-	text="${GREEN}Available Commands:
-		add  - add a record
-		file - check if users.db file exists
-		list - show all records
-		help - list of commands
-		${NORMAL}
-	"
+DB_USER_FILE=../data/users.db
 
-	printf "%s\n" "$text"
-}
-
-function confirmation(){
+confirmation() {
 	local _prompt _default _response
 
-  if [ "$1" ]; then _prompt="$1"; else _prompt="Are you sure"; fi
-  _prompt="$_prompt [y/n] ?"
+	if [ "$1" ]; then _prompt="$1"; else _prompt="Are you sure"; fi
+	_prompt="$_prompt [y/n] ?"
 
-  # Loop forever until the user enters a valid response (Y/N or Yes/No).
-  while true; do
-    read -r -p "$_prompt " _response
-    case "$_response" in
-      [Yy][Ee][Ss]|[Yy]) # Yes or Y (case-insensitive).
-        return 0
-        ;;
-      [Nn][Oo]|[Nn])  # No or N.
-        return 1
-        ;;
-      *) # Anything else (including a blank) is invalid.
-        ;;
-    esac
-  done
-  }
+	# Loop forever until the user enters a valid response (Y/N or Yes/No).
+	while true; do
+		read -r -p "$_prompt " _response
+		case "$_response" in
+		[Yy][Ee][Ss] | [Yy]) # Yes or Y (case-insensitive).
+			printf "%s\n" "$_response"
+			return 0
+			;;
+		[Nn][Oo] | [Nn]) # No or N.
+			printf "%s\n" "$_response"
+			return 1
+			;;
+		*) # Anything else (including a blank) is invalid.
+			;;
+		esac
+	done
+}
 
-function is_file_exists() {
-	DB_USER_FILE=../data/users.db
-	local CREATE_FILE=false user_response
-	if [ ! -f "$DB_USER_FILE" ]; then
-		printf "%s\n" "File '$DB_USER_FILE' exists."
+is_file_exists() {
+	if [ -f $DB_USER_FILE ]; then
+		printf "%s\n" "$GREEN File '$DB_USER_FILE' exists.$NORMAL"
+		return 0
 	else
-		#printf "%s\n" "$RED $DB_USER_FILE doesn't exist. Creating...\n$NORMAL" > $DB_USER_FILE
-		printf "%s\n" "$RED $DB_USER_FILE doesn't exist. Creating...\n$NORMAL"
-
- 		user_response=$(confirmation "$@")
- 		echo "Result $user_response"
-		#read -p "Create a new file [y]es | [n]o :" CREATE_FILE
-
-		printf "" > $DB_USER_FILE
+		printf "%s\n" "$RED File $DB_USER_FILE doesn't exist. $NORMAL" >&2
+		return 1
 	fi
 }
 
-function get_user_input() {
-	local USER_INPUT
-	unset USER_INPUT
-	while [[ ! ${USER_INPUT} =~ ^[a-zA-Z]+$ ]]; do
-		read -p "$1" USER_INPUT
-		printf "%s\n" "$RED Only Latin letters are allowed.$GREEN" >&2
-	done
-	#printf "%s\n" "$USER_INPUT"
-	#local func_result=$USER_INPUT
-	#echo "$func_result"
+create_file() {
+	local _file_created
+
+	if [ -f $DB_USER_FILE ]; then
+		printf "%s\n" "$GREEN File '$DB_USER_FILE' exists.$NORMAL"
+	else
+		printf "%s\n" "$RED File $DB_USER_FILE doesn't exist. File created. $NORMAL" >&2
+		printf "" >$DB_USER_FILE
+	fi
 }
 
-function add_user() {
+get_user_input() {
+	local _user_input
+	unset _user_input
+	while true; do
+		read -p "$GREEN $1" _user_input
+		if [[ ! ${_user_input} =~ ^[a-zA-Z]+$ ]]; then
+			printf "%s\n" "$RED Only Latin letters are allowed.$GREEN" >&2
+		else
+			printf "%s\n" "$_user_input $NORMAL"
+			break
+		fi
+	done
+}
+
+add_user() {
 	printf "%s\n" "$GREEN Adding a new user..."
 
-	is_file_exists
+	create_file
 
 	USER_NAME="$(get_user_input 'User name: ')"
 	USER_ROLE="$(get_user_input 'User role:  ')"
 
-	#printf "%s\n" "$USER_NAME, $USER_ROLE" >> $DB_USER_FILE
+	printf "%s\n" "$USER_NAME, $USER_ROLE" >>"$DB_USER_FILE"
+	printf "%s" "$NORMAL"
+}
 
-	printf "%s\n" "$NORMAL"
-	exit 1
-	#func_result="$(check_input)"
-	#echo $func_result
-	#read -p "User role: " USER_ROLE
-
-	#printf "%s User - " "$USER_INPUT" "$USER_ROLE is created." "$NORMAL"
+print_available_commands() {
+	text="${GREEN}Available Commands:
+		add  - add a record
+		file - check if users.db file exists
+		list - show all records
+		conf - confirmation
+		val - user validation input
+		help - list of commands
+		${NORMAL}
+	"
+	printf "%s\n" "$text"
 }
 
 if [ $# -eq 0 ]; then
 	print_available_commands
-	exit 1
 fi
 
-echo "$0 start"
 while [ -n "$1" ]; do
 	case "$1" in
 	add) add_user ;;
-	conf) confirmation ;;
+	conf) confirmation "Create a file" ;;
+	create) create_file ;;
 	list) echo "Found the list option" ;;
+	file) is_file_exists "$@" ;;
+	val) get_user_input "User input: " ;;
 	* | help) print_available_commands ;;
 	esac
 	shift
